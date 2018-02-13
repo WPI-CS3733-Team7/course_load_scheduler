06618ed4d3eef27b77dd82bce558e5db809f9038
leifsahyun <lgsahyun@wpi.edu>
2018-02-12 21:29:27 -0500

    Created XML Layout for left half of scheduler

--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/MainViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/MainViewImpl.java
@@ -14,6 +14,7 @@ import com.google.gwt.user.client.ui.Widget;
 public class MainViewImpl extends Composite implements HasText {
 
 	private static MainViewImplUiBinder uiBinder = GWT.create(MainViewImplUiBinder.class);
+	@UiField Button button;
 
 	interface MainViewImplUiBinder extends UiBinder<Widget, MainViewImpl> {
 	}
@@ -36,4 +37,7 @@ public class MainViewImpl extends Composite implements HasText {
 		return null;
 	}
 
+	@UiHandler("button")
+	void onButtonClick(ClickEvent event) {
+	}
 }
