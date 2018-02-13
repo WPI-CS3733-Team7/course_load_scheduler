630cf80bb58b3c97e6f4ce68adb4d8c24991d2eb
Marcus <marcuschan0311@gmail.com>
2018-02-12 22:45:01 -0500

    request for user in mainview and admin in admin view

--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/MainViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/MainViewImpl.java
@@ -10,10 +10,14 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.RadioButton;
 
 public class MainViewImpl extends Composite implements HasText {
 
 	private static MainViewImplUiBinder uiBinder = GWT.create(MainViewImplUiBinder.class);
+	@UiField RadioButton rdoCourse;
+	@UiField RadioButton rdoOther;
+	@UiField Button rdoSubmit;
 
 	interface MainViewImplUiBinder extends UiBinder<Widget, MainViewImpl> {
 	}
