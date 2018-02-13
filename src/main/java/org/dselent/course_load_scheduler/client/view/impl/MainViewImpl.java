a55ee66c5cbac713abbbd85b865da0b6d114969a
John Amaral <john3r8amaral@yahoo.com>
2018-02-13 18:11:59 -0500

    Added other schedule UI fields to ScheduleViewImpl

--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/MainViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/MainViewImpl.java
@@ -9,27 +9,27 @@ import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.ui.RadioButton;
 
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ListBox;
+//import com.google.gwt.user.client.ui.Label;
+//import com.google.gwt.user.client.ui.ListBox;
 
 public class MainViewImpl extends Composite implements HasText {
 
 	private static MainViewImplUiBinder uiBinder = GWT.create(MainViewImplUiBinder.class);
-	@UiField Button button;
-	@UiField Button button_1;
+	//@UiField Button button;
+	//@UiField Button button_1;
 
 
 	@UiField RadioButton rdoCourse;
 	@UiField RadioButton rdoOther;
 	@UiField Button rdoSubmit;
 
-	@UiField Button createSection;
+	/*@UiField Button createSection;
 	@UiField Button editSection;
 	@UiField Button validate;
 	@UiField Label selectYear;
 	@UiField Label selectTerm;
 	@UiField ListBox yearSelect;
-	@UiField ListBox termSelect;
+	@UiField ListBox termSelect;*/
 
 
 
@@ -57,8 +57,8 @@ public class MainViewImpl extends Composite implements HasText {
 
 
 
-	}
-	@UiHandler("button_1")
-	void onButton_1Click(ClickEvent event) {
-	}
+	//}
+	//@UiHandler("button_1")
+	//void onButton_1Click(ClickEvent event) {
+	//}
 }
