f567ce033b0cc70fe248c94eeae987bc29a5ea00
John Amaral <john3r8amaral@yahoo.com>
2018-02-13 22:05:52 -0500

    SchedulePresenter files, ScheduleView files, added ScheduleView XML

---
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/SchedulePresenterImpl.java
@@ -0,0 +1,43 @@
+package org.dselent.course_load_scheduler.client.presenter.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dselent.course_load_scheduler.client.presenter.BasePresenter;
+import org.dselent.course_load_scheduler.client.presenter.IndexPresenter;
+import org.dselent.course_load_scheduler.client.presenter.SchedulePresenter;
+import org.dselent.course_load_scheduler.client.view.BaseView;
+import org.dselent.course_load_scheduler.client.view.ScheduleView;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.inject.Inject;
+
+public class SchedulePresenterImpl extends BasePresenterImpl implements SchedulePresenter {
+	
+	private IndexPresenter parentPresenter;
+
+	@Override
+	public void go(HasWidgets container) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BaseView<? extends BasePresenter> getView() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IndexPresenter getParentPresenter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setParentPresenter(IndexPresenter parentPresenter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/org/dselent/course_load_scheduler/client/view/ScheduleView.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/ScheduleView.java
@@ -1,8 +1,10 @@
 package org.dselent.course_load_scheduler.client.view;
 
+import org.dselent.course_load_scheduler.client.presenter.SchedulePresenter;
+
 import com.google.gwt.user.client.ui.Button;
 
-public interface ScheduleView {
+public interface ScheduleView extends BaseView<SchedulePresenter>{
 	public void addInstructorButton(String instructorName);
 	public void addCourseButton(String courseName);
 	public void removeInstructorButton(String instructorName);
