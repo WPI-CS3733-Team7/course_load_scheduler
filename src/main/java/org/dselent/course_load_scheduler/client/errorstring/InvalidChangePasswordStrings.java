ab61556857c51c6343cccbeeb0a7c5bba67cff3c
jlharris <justinlharris15@gmail.com>
2018-02-14 18:00:51 -0500

    Finished changePassword and added more invalid change password strings

--- src/main/java/org/dselent/course_load_scheduler/client/errorstring/InvalidChangePasswordStrings.java
+++ src/main/java/org/dselent/course_load_scheduler/client/errorstring/InvalidChangePasswordStrings.java
@@ -2,6 +2,9 @@ package org.dselent.course_load_scheduler.client.errorstring;
 
 public class InvalidChangePasswordStrings {
 	
+	public static final String BAD_NEW_PASSWORD = "Your new password cannot be your old password.";
+	public static final String BAD_PASSWORD_LENGTH = "Your password must be between 8 and 20 characters.";
+	public static final String BAD_CONFIRM_NEW_PASSWORD = "Your confirmed new password must be the same as your new password.";
 	public static final String NULL_OLD_PASSWORD = "Your old password cannot be empty.";
 	public static final String NULL_NEW_PASSWORD = "Your new password cannot be empty.";
 	public static final String NULL_CONFIRM_NEW_PASSWORD = "Your confirmed new password cannot be empty.";
