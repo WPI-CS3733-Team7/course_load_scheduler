7c4a97758c736b7d8a2d1880acbcc2b21de62661
jlharris <justinlharris15@gmail.com>
2018-02-14 14:40:46 -0500

    Added InvalidChangePasswordStrings

--- src/main/java/org/dselent/course_load_scheduler/client/errorstring/InvalidChangePasswordStrings.java
+++ src/main/java/org/dselent/course_load_scheduler/client/errorstring/InvalidChangePasswordStrings.java
@@ -2,9 +2,9 @@ package org.dselent.course_load_scheduler.client.errorstring;
 
 public class InvalidChangePasswordStrings {
 	
-	public static final NULL_OLD_PASSWORD = "Your old password cannot be empty.";
-	public static final NULL_NEW_PASSWORD = "Your new password cannot be empty.";
-	public static final NULL_CONFIRM_NEW_PASSWORD = "Your confirmed new password cannot be empty.";
+	public static final String NULL_OLD_PASSWORD = "Your old password cannot be empty.";
+	public static final String NULL_NEW_PASSWORD = "Your new password cannot be empty.";
+	public static final String NULL_CONFIRM_NEW_PASSWORD = "Your confirmed new password cannot be empty.";
 	
 
 }
