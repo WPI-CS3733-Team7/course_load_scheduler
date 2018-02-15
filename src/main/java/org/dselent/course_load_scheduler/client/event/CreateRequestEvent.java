9df85d713314814d3666c025ae113c41229030d9
kimiguo <kimiguo@autoreg-125625.dyn.wpi.edu>
2018-02-14 19:50:46 -0500

    Create Request and Send Reply Events

--- src/main/java/org/dselent/course_load_scheduler/client/event/CreateRequestEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/CreateRequestEvent.java
@@ -1,5 +1,37 @@
 package org.dselent.course_load_scheduler.client.event;
 
-public class CreateRequestEvent {
+import org.dselent.course_load_scheduler.client.action.CreateRequestAction;
+import org.dselent.course_load_scheduler.client.event_handler.CreateRequestEventHandler;
+import org.dselent.course_load_scheduler.client.event_handler.SelectRequestEventHandler;
 
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.GwtEvent.Type;
+
+public class CreateRequestEvent extends GwtEvent<CreateRequestEventHandler>{
+	public static Type<CreateRequestEventHandler> TYPE = new Type<CreateRequestEventHandler>();
+	
+	private CreateRequestAction action;
+	
+	public CreateRequestEvent(CreateRequestAction action)
+	{
+		this.action = action;
+	}
+	
+	public CreateRequestAction getAction()
+	{
+		return action;
+	}
+	
+	
+	@Override
+	public Type<CreateRequestEventHandler> getAssociatedType()
+	{
+		return TYPE;
+	}
+
+	@Override
+	protected void dispatch(CreateRequestEventHandler handler)
+	{
+		handler.onCreateRequest(this);
+	}
 }
--- src/main/java/org/dselent/course_load_scheduler/client/event/SendReplyEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/SendReplyEvent.java
@@ -1,5 +1,38 @@
 package org.dselent.course_load_scheduler.client.event;
 
-public class SendReplyEvent {
+import org.dselent.course_load_scheduler.client.action.SendReplyAction;
+import org.dselent.course_load_scheduler.client.event_handler.SelectRequestEventHandler;
+import org.dselent.course_load_scheduler.client.event_handler.SendReplyEventHandler;
 
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.GwtEvent.Type;
+
+public class SendReplyEvent extends GwtEvent<SendReplyEventHandler>{
+	
+	public static Type<SendReplyEventHandler> TYPE = new Type<SendReplyEventHandler>();
+	
+	private SendReplyAction action;
+	
+	public SendReplyEvent(SendReplyAction action)
+	{
+		this.action = action;
+	}
+	
+	public SendReplyAction getAction()
+	{
+		return action;
+	}
+	
+	
+	@Override
+	public Type<SendReplyEventHandler> getAssociatedType()
+	{
+		return TYPE;
+	}
+
+	@Override
+	protected void dispatch(SendReplyEventHandler handler)
+	{
+		handler.onSendReply(this);
+	}
 }
