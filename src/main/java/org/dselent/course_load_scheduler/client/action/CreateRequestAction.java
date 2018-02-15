3e6faf650a58bad1f3b8c4ea8184fc2b208d5557
kimiguo <kimiguo@autoreg-125625.dyn.wpi.edu>
2018-02-14 19:28:36 -0500

    CreateRequest

---
+++ src/main/java/org/dselent/course_load_scheduler/client/action/CreateRequestAction.java
@@ -0,0 +1,18 @@
+package org.dselent.course_load_scheduler.client.action;
+
+public class CreateRequestAction {
+	private String requestType;
+	
+	public CreateRequestAction(String requestType) {
+		this.requestType = requestType;
+	}
+
+	public String getrequestType() {
+		return requestType;
+	}
+	
+	@Override
+	public String toString() {
+		return "CreateRequestAction [requestType=" + requestType + "]";
+	}	
+}
