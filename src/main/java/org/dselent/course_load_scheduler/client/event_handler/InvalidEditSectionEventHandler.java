86eece368ad850ada875139af58598cfdf124362
John Amaral <john3r8amaral@yahoo.com>
2018-02-14 19:29:21 -0500

    Added InvalidEditSection files, updated SchedulePresenter and Views

---
+++ src/main/java/org/dselent/course_load_scheduler/client/event_handler/InvalidEditSectionEventHandler.java
@@ -0,0 +1,10 @@
+package org.dselent.course_load_scheduler.client.event_handler;
+
+import org.dselent.course_load_scheduler.client.event.InvalidEditSectionEvent;
+import com.google.gwt.event.shared.EventHandler;
+
+public interface InvalidEditSectionEventHandler extends EventHandler {
+	
+	public void onInvalidLogin(InvalidEditSectionEvent evt);
+
+}
