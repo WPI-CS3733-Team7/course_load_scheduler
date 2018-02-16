fcdce28947539a9ab0ed3bb4e3e995db7d433ab4
kimikobe <jguo2@wpi.edu>
2018-02-16 16:23:33 -0500

    Add files via upload

---
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/UserRequestViewImpl.java
@@ -0,0 +1,160 @@
+package org.dselent.course_load_scheduler.client.view.impl;
+
+import org.dselent.course_load_scheduler.client.presenter.UserRequestPresenter;
+import org.dselent.course_load_scheduler.client.view.UserRequestView;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ListBox;
+
+public class UserRequestViewImpl extends BaseViewImpl<UserRequestPresenter> implements UserRequestView {
+
+	private static UserRequestViewImplUiBinder uiBinder = GWT.create(UserRequestViewImplUiBinder.class);
+	@UiField 
+	Label createLabel;
+	
+	@UiField 
+	HorizontalPanel requestTabPanel;
+	
+	@UiField 
+	VerticalPanel createPanel;
+	
+	@UiField 
+	VerticalPanel requestHistoryPanel;
+	
+	@UiField 
+	Label requestTypeLabel;
+	
+	@UiField 
+	RadioButton courseRdo;
+	
+	@UiField 
+	RadioButton otherRdo;
+	
+	@UiField 
+	TextArea descriptTextArea;
+	
+	@UiField 
+	Button submitButton;
+	
+	@UiField 
+	Label requestHistoryLabel;
+	
+	@UiField 
+	ListBox requestHistoryList;
+
+	interface UserRequestViewImplUiBinder extends UiBinder<Widget, UserRequestViewImpl> {
+	}
+
+	public UserRequestViewImpl() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+	
+	@Override
+	public void setPresenter(UserRequestPresenter presenter) {
+		// TODO Auto-generated method stub
+		this.presenter = presenter;
+		
+	}
+
+
+
+	@Override
+	public RadioButton getCourseRdo() {
+		// TODO Auto-generated method stub
+		
+		return courseRdo;
+	}
+
+
+
+	@Override
+	public void setCourseRdo(RadioButton courseRdo) {
+		// TODO Auto-generated method stub
+		this.courseRdo = courseRdo;
+	}
+
+
+
+	@Override
+	public RadioButton getOtherRdo() {
+		// TODO Auto-generated method stub
+		return otherRdo;
+	}
+
+
+
+	@Override
+	public void setOtherRdo(RadioButton otherRdo) {
+		// TODO Auto-generated method stub
+		this.otherRdo = otherRdo;
+	}
+
+
+
+	@Override
+	public Button getSubmitButton() {
+		// TODO Auto-generated method stub
+		return submitButton;
+	}
+	
+	@Override
+	public ListBox getRequestHistoryList() {
+		return requestHistoryList;
+	}
+	
+	@Override
+	public void setRequestHistoryList(ListBox requestHistoryList) {
+		this.requestHistoryList = requestHistoryList;
+	}
+	
+	@Override
+	public Widget getWidgetContainer()
+	{
+		return this;
+	}
+	
+	@Override
+	public HasWidgets getViewRootPanel()
+	{
+		return requestTabPanel;
+	}
+	
+	@Override
+	public void showErrorMessages(String errorMessages)
+	{
+		Window.alert(errorMessages);
+	}
+	
+	@UiHandler("submitButton")
+	void onSubmitButtonClicked(ClickEvent evt)
+	{
+		presenter.submit();
+	}
+
+	@Override
+	public TextBox getDescriptionTextBox() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDescriptionText(TextBox descriptionTextBox) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
