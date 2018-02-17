09ad2eb58cfb685a7867dfa7a86c95b4efd5c911
kimikobe <jguo2@wpi.edu>
2018-02-17 08:29:57 -0500

    Delete InvalidSubmitStrings.java

--- src/main/java/org/dselent/course_load_scheduler/client/errorstring/InvalidSubmitStrings.java
+++
@@ -1,5 +0,0 @@
-package org.dselent.course_load_scheduler.client.errorstring;
-
-public class InvalidSubmitStrings {
-	public static final String NULL_REQUESTTYPE = "Your request type cannot be empty.";
-}
