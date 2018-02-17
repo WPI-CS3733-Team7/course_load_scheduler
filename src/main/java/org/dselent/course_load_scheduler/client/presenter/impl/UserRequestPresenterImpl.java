5aa95a61889372d142b3111651b0115e285b1718
kimikobe <jguo2@wpi.edu>
2018-02-17 08:29:26 -0500

    Add files via upload

---
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/UserRequestPresenterImpl.java
@@ -0,0 +1,160 @@
+package org.dselent.course_load_scheduler.client.presenter.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dselent.course_load_scheduler.client.action.InvalidRequestAction;
+import org.dselent.course_load_scheduler.client.action.SendRequestAction;
+import org.dselent.course_load_scheduler.client.errorstring.InvalidSubmitStrings;
+import org.dselent.course_load_scheduler.client.event.InvalidRequestEvent;
+import org.dselent.course_load_scheduler.client.event.SendRequestEvent;
+import org.dselent.course_load_scheduler.client.event_handler.InvalidRequestEventHandler;
+import org.dselent.course_load_scheduler.client.exceptions.EmptyStringException;
+import org.dselent.course_load_scheduler.client.presenter.BasePresenter;
+import org.dselent.course_load_scheduler.client.presenter.IndexPresenter;
+import org.dselent.course_load_scheduler.client.presenter.UserRequestPresenter;
+import org.dselent.course_load_scheduler.client.view.BaseView;
+import org.dselent.course_load_scheduler.client.view.UserRequestView;
+
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.inject.Inject;
+
+public class UserRequestPresenterImpl extends BasePresenterImpl implements UserRequestPresenter, InvalidRequestEventHandler{
+	private IndexPresenter parentPresenter;
+	private UserRequestView view;
+	private boolean submitClickInProgress;
+	
+	@Inject
+	public UserRequestPresenterImpl(IndexPresenter parentPresenter, UserRequestView view)
+	{
+		this.view = view;
+		this.parentPresenter = parentPresenter;
+		view.setPresenter(this);
+		submitClickInProgress = false;
+	}
+	
+	@Override
+	public void init()
+	{
+		bind();
+	}
+
+	@Override
+	public void bind()
+	{
+		HandlerRegistration registration;
+		
+		registration = eventBus.addHandler(InvalidRequestEvent.TYPE, this);
+		eventBusRegistration.put(InvalidRequestEvent.TYPE, registration);
+	}
+	
+	@Override
+	public void go(HasWidgets container) {
+		// TODO Auto-generated method stub
+		container.clear();
+		container.add(view.getWidgetContainer());
+	}
+
+	@Override
+	public BaseView<? extends BasePresenter> getView() {
+		// TODO Auto-generated method stub
+		return view;
+	}
+
+	@Override
+	public IndexPresenter getParentPresenter() {
+		// TODO Auto-generated method stub
+		return parentPresenter;
+	}
+
+	@Override
+	public void setParentPresenter(IndexPresenter parentPresenter) {
+		// TODO Auto-generated method stub
+		this.parentPresenter = parentPresenter;
+	}
+
+	@Override
+	public void submit() {
+		// TODO Auto-generated method stub
+		if(!submitClickInProgress)
+		{
+			submitClickInProgress = true;
+			view.getSubmitButton().setEnabled(false);
+			parentPresenter.showLoadScreen();
+			boolean courses = view.getCourseRdo().getValue(); //Check click or not on radio button course
+			boolean other = view.getOtherRdo().getValue(); ////Check click or not on radio button other
+			String requestType = "";
+			
+			String description = view.getDescriptionTextArea().getText();
+			
+			boolean validRequesttype = true; //Manage request type
+			boolean validDescription = true; //Manage description
+
+			List<String> invalidReasonList = new ArrayList<>();
+			
+			try
+			{
+				validateField(description);
+			}
+			catch(EmptyStringException e) //Empty description
+			{
+				invalidReasonList.add(InvalidSubmitStrings.NULL_DESCRIPTION); //Null description error message
+				validDescription = false;
+			}
+			
+			if(courses) {
+				requestType = "courses"; //return courses if clicked course radio button
+			}
+			else if(other){
+				requestType = "others"; ////return other if clicked other radio button
+			}
+			else {
+				requestType = null; //None of radio button selected
+				invalidReasonList.add(InvalidSubmitStrings.NULL_REQUESTTYPE); //Null request Type error message
+				validRequesttype = false;
+			}
+			
+			if(validRequesttype && validDescription) { //Both True
+				sendRequest(description, requestType); //Show both informations
+			}
+			else									//Return error messages
+			{
+				InvalidRequestAction ira = new InvalidRequestAction(invalidReasonList);
+				InvalidRequestEvent ire = new InvalidRequestEvent(ira);
+				eventBus.fireEvent(ire);
+			}
+		}
+	}
+	
+	public void onInvalidRequest(InvalidRequestEvent evt)
+	{
+		parentPresenter.hideLoadScreen();
+		view.getSubmitButton().setEnabled(true);
+		submitClickInProgress = false;
+		
+		InvalidRequestAction ira = evt.getAction();
+		view.showErrorMessages(ira.toString());
+	}
+	
+	private void sendRequest(String Description, String Requesttype)
+	{
+		SendRequestAction sra = new SendRequestAction(Description, Requesttype);
+		SendRequestEvent sre = new SendRequestEvent(sra);
+		eventBus.fireEvent(sre);
+	}
+	
+	private void validateField(String field) throws EmptyStringException
+	{
+		checkEmptyString(field);
+	}
+	
+	private void checkEmptyString(String string) throws EmptyStringException
+	{
+		if(string == null || string.equals(""))
+		{
+			throw new EmptyStringException();
+		}
+	}
+
+}
