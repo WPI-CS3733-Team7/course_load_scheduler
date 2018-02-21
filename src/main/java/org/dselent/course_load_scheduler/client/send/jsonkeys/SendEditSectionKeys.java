461a37852d31b3783926df4656d286e61b4c5c12
John Amaral <john3r8amaral@yahoo.com>
2018-02-20 23:01:07 -0500

    Added receive and send keys for schedule tab, translators

---
+++ src/main/java/org/dselent/course_load_scheduler/client/send/jsonkeys/SendEditSectionKeys.java
@@ -0,0 +1,13 @@
+package org.dselent.course_load_scheduler.client.send.jsonkeys;
+
+public enum SendEditSectionKeys {
+	SECTION_NAME,
+	SECTION_ID,
+	SECTION_TYPE,
+	POPULATION,
+	YEAR,
+	TERM,
+	DAYS,
+	START_TIME,
+	END_TIME;
+}
--- src/main/java/org/dselent/course_load_scheduler/client/translator/impl/EditSectionActionTranslatorImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/translator/impl/EditSectionActionTranslatorImpl.java
@@ -1,5 +1,76 @@
 package org.dselent.course_load_scheduler.client.translator.impl;
 
-public class EditSectionActionTranslatorImpl {
+import java.util.Date;
 
+import org.dselent.course_load_scheduler.client.action.ReceiveEditSectionAction;
+import org.dselent.course_load_scheduler.client.action.SendEditSectionAction;
+import org.dselent.course_load_scheduler.client.model.CourseSection;
+import org.dselent.course_load_scheduler.client.model.CalendarInfo;
+import org.dselent.course_load_scheduler.client.receive.jsonkeys.ReceiveEditSectionKeys;
+import org.dselent.course_load_scheduler.client.send.jsonkeys.SendEditSectionKeys;
+import org.dselent.course_load_scheduler.client.translator.ActionTranslator;
+import org.dselent.course_load_scheduler.client.utils.JSONHelper;
+
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONValue;
+
+public class EditSectionActionTranslatorImpl implements ActionTranslator<SendEditSectionAction, ReceiveEditSectionAction>
+{
+	@Override
+	public JSONObject translateToJson(SendEditSectionAction action)
+	{
+		JSONObject jsonObject = new JSONObject();
+		
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(SendEditSectionKeys.SECTION_NAME), action.getSectionName());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(SendEditSectionKeys.SECTION_ID), action.getSectionId());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(SendEditSectionKeys.SECTION_TYPE), action.getSectionType());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(SendEditSectionKeys.POPULATION), action.getPopulation());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(SendEditSectionKeys.YEAR), action.getYear());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(SendEditSectionKeys.TERM), action.getTerm());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(SendEditSectionKeys.DAYS), action.getDays());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(SendEditSectionKeys.START_TIME), action.getStartTime());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(SendEditSectionKeys.END_TIME), action.getEndTime());
+		
+		return jsonObject;
+	}
+	
+	@Override
+	public ReceiveEditSectionAction translateToAction(JSONObject json)
+	{		
+		// null values will not have their keys sent back from the sever
+		// this will throw an exception here
+		// you may choose to handle the exception as you wish
+		
+		// sent timestamps as epoch seconds (long)
+		
+		JSONValue jsonObject = json.get("success");
+		JSONObject courseSectionObject = jsonObject.isArray().get(0).isObject();
+		
+		Integer id = JSONHelper.getIntValue(userObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.ID));
+		String userName = JSONHelper.getStringValue(userObject, JSONHelper.convertKeyName(ReceiveLoginKeys.USER_NAME));
+		String firstName = JSONHelper.getStringValue(userObject, JSONHelper.convertKeyName(ReceiveLoginKeys.FIRST_NAME));
+		String lastName = JSONHelper.getStringValue(userObject, JSONHelper.convertKeyName(ReceiveLoginKeys.LAST_NAME));
+		String email = JSONHelper.getStringValue(userObject, JSONHelper.convertKeyName(ReceiveLoginKeys.EMAIL));
+		Integer userStateId = JSONHelper.getIntValue(userObject, JSONHelper.convertKeyName(ReceiveLoginKeys.USER_STATE_ID));
+		Long createdAt = JSONHelper.getLongValue(userObject, JSONHelper.convertKeyName(ReceiveLoginKeys.CREATED_AT));
+		Long updatedAt = JSONHelper.getLongValue(userObject, JSONHelper.convertKeyName(ReceiveLoginKeys.UPDATED_AT));
+		
+		// TODO look into time conversion more
+		// put into JSONHelper?
+		
+		User user = new User();
+		user.setId(id);
+		user.setUserName(userName);
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setEmail(email);
+		user.setUserStateId(userStateId);
+		user.setCreatedAt(new Date(createdAt));
+		user.setUpdatedAt(new Date(updatedAt));
+		
+		// possibly use builder pattern if it is a lot of data
+		ReceiveLoginAction action = new ReceiveLoginAction(user);	
+	
+		return action;
+	}
 }
