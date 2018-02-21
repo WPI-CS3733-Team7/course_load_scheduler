848195dba02e1047b3f4f92e4bee77b5d6885e0e
jlanson <jlanson@autoreg-078133.dyn.wpi.edu>
2018-02-20 20:07:30 -0500

    Added account service impl class

---
+++ src/main/java/org/dselent/course_load_scheduler/client/service/impl/AccountServiceImpl.java
@@ -0,0 +1,8 @@
+package org.dselent.course_load_scheduler.client.service.impl;
+
+import org.dselent.course_load_scheduler.client.service.AccountService;
+
+public class AccountServiceImpl extends BaseServiceImpl implements AccountService
+{
+
+}
