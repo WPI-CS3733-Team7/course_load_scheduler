8e183006f672aef6b99263d8aad571b4e23e8fe2
jlharris <justinlharris15@gmail.com>
2018-02-20 22:17:32 -0500

    Implemented Change Password

--- src/main/java/org/dselent/course_load_scheduler/client/service/impl/AccountServiceImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/service/impl/AccountServiceImpl.java
@@ -1,10 +1,18 @@
 package org.dselent.course_load_scheduler.client.service.impl;
 
+import org.dselent.course_load_scheduler.client.action.SendChangePasswordAction;
+import org.dselent.course_load_scheduler.client.callback.SendChangePasswordCallback;
+import org.dselent.course_load_scheduler.client.callback.SendRegisterCallback;
 import org.dselent.course_load_scheduler.client.event.SendChangePasswordEvent;
 import org.dselent.course_load_scheduler.client.event.SendEditUserEvent;
+import org.dselent.course_load_scheduler.client.network.NetworkRequest;
+import org.dselent.course_load_scheduler.client.network.NetworkRequestStrings;
 import org.dselent.course_load_scheduler.client.service.AccountService;
+import org.dselent.course_load_scheduler.client.translator.impl.ChangePasswordActionTranslatorImpl;
+import org.dselent.course_load_scheduler.client.translator.impl.RegisterActionTranslatorImpl;
 
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.json.client.JSONObject;
 
 public class AccountServiceImpl extends BaseServiceImpl implements AccountService
 {
@@ -34,9 +42,15 @@ public class AccountServiceImpl extends BaseServiceImpl implements AccountServic
 	@Override
 	public void onSendChangePassword(SendChangePasswordEvent evt)
 	{
+		SendChangePasswordAction action = evt.getAction();
+		ChangePasswordActionTranslatorImpl changePasswordActionTranslator = new ChangePasswordActionTranslatorImpl();
+		JSONObject json = changePasswordActionTranslator.translateToJson(action);
+		SendChangePasswordCallback changePasswordCallback = new SendChangePasswordCallback(eventBus, evt.getContainer());
 		
+		NetworkRequest request = new NetworkRequest(NetworkRequestStrings.REGISTER, changePasswordCallback, json);
+		request.send();
 	}
-	
+		
 	@Override
 	public void onSendEditUser(SendEditUserEvent evt)
 	{
