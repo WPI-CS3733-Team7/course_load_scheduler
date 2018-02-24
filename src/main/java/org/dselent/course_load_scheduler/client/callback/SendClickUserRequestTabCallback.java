81d881b1ef298674dd3d401e3cb47f57d5733200
kimiguo <kimiguo@autoreg-125625.dyn.wpi.edu>
2018-02-23 22:53:04 -0500

    UserClickRequestTabEvent/Handler

--- src/main/java/org/dselent/course_load_scheduler/client/callback/SendClickUserRequestTabCallback.java
+++ src/main/java/org/dselent/course_load_scheduler/client/callback/SendClickUserRequestTabCallback.java
@@ -1,8 +1,8 @@
 package org.dselent.course_load_scheduler.client.callback;
 
-import org.dselent.course_load_scheduler.client.action.ReceiveClickRequestTabAction;
-import org.dselent.course_load_scheduler.client.event.ReceiveClickRequestTabEvent;
-import org.dselent.course_load_scheduler.client.translator.impl.ClickRequestTabActionTranslatorImpl;
+import org.dselent.course_load_scheduler.client.action.ReceiveClickUserRequestTabAction;
+import org.dselent.course_load_scheduler.client.event.ReceiveUserClickRequestTabEvent;
+import org.dselent.course_load_scheduler.client.translator.impl.ClickUserRequestTabActionTranslatorImpl;
 import org.dselent.course_load_scheduler.client.utils.JSONHelper;
 
 import com.google.gwt.event.shared.SimpleEventBus;
@@ -30,7 +30,7 @@ public class SendClickUserRequestTabCallback extends DisplayCallback<JSONValue>{
 		ClickUserRequestTabActionTranslatorImpl ClickUserRequestTabActionTranslator = new ClickUserRequestTabActionTranslatorImpl();
 		ReceiveClickUserRequestTabAction action = ClickUserRequestTabActionTranslator.translateToAction(json);
 		
-		ReceiveClickUserRequestTabEvent event = new ReceiveClickUserRequestTabEvent(action, getContainer());
+		ReceiveUserClickRequestTabEvent event = new ReceiveUserClickRequestTabEvent(action, getContainer());
 		eventBus.fireEvent(event);
 	}
 	
--- src/main/java/org/dselent/course_load_scheduler/client/event_handler/ReceiveUserClickRequestTabEventHandler.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event_handler/ReceiveUserClickRequestTabEventHandler.java
@@ -1,7 +1,9 @@
 package org.dselent.course_load_scheduler.client.event_handler;
 
-import org.dselent.course_load_scheduler.client.event.ReceiveClickRequestTabEvent;
+import org.dselent.course_load_scheduler.client.event.ReceiveUserClickRequestTabEvent;
 
-public interface ReceiveUserClickRequestTabEventHandler {
-	public void onReceiveClickRequestTab(ReceiveClickRequestTabEvent evt);
+import com.google.gwt.event.shared.EventHandler;
+
+public interface ReceiveUserClickRequestTabEventHandler extends EventHandler{
+	public void onReceiveUserClickRequestTab(ReceiveUserClickRequestTabEvent evt);
 }
