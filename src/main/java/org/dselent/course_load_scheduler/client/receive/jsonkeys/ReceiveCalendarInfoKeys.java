ca8c476711e4dcbce5c7fa47f20cab4898514ff4
leifsahyun <lgsahyun@wpi.edu>
2018-02-26 02:37:33 -0500

    Still working on course sections

---
+++ src/main/java/org/dselent/course_load_scheduler/client/receive/jsonkeys/ReceiveCalendarInfoKeys.java
@@ -0,0 +1,13 @@
+package org.dselent.course_load_scheduler.client.receive.jsonkeys;
+
+public enum ReceiveCalendarInfoKeys {
+	
+	MESSAGE,
+	
+	ID,	
+	CAL_YEAR,
+	CAL_TERM,
+	DAYS,
+	START_TIME,
+	END_TIME;	
+}
--- src/main/java/org/dselent/course_load_scheduler/client/translator/impl/EditSectionActionTranslatorImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/translator/impl/EditSectionActionTranslatorImpl.java
@@ -4,6 +4,7 @@ import org.dselent.course_load_scheduler.client.action.ReceiveEditSectionAction;
 import org.dselent.course_load_scheduler.client.action.SendEditSectionAction;
 import org.dselent.course_load_scheduler.client.model.CourseSection;
 import org.dselent.course_load_scheduler.client.model.CalendarInfo;
+import org.dselent.course_load_scheduler.client.receive.jsonkeys.ReceiveCalendarInfoKeys;
 import org.dselent.course_load_scheduler.client.receive.jsonkeys.ReceiveEditSectionKeys;
 import org.dselent.course_load_scheduler.client.send.jsonkeys.SendEditSectionKeys;
 import org.dselent.course_load_scheduler.client.translator.ActionTranslator;
@@ -60,66 +61,56 @@ public class EditSectionActionTranslatorImpl implements ActionTranslator<SendEdi
 		//CourseSection object
 		
 		JSONValue courseSectionObjectStart = returnEditSectionObject.get("courseSection");
-		JSONArray courseSectionListObject = courseSectionObjectStart.isArray();
+		JSONObject courseSectionObject = courseSectionObjectStart.isObject();
 		
 		CourseSection courseSection = new CourseSection();
 		
-		for (int i = 0; i < courseSectionListObject.size(); i++) {
+		Integer id = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.ID));
+		String sectionName = JSONHelper.getStringValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.SECTION_NAME));
+		Integer sectionId = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.SECTION_ID));
+		String sectionType = JSONHelper.getStringValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.SECTION_TYPE));
+		Integer population = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.POPULATION));
+		Integer courseId = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.COURSE_ID));
+		Integer instructorId = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.INSTRUCTOR_ID));
+		Integer calendarInfoId = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.CALENDAR_INFO_ID));
 		
-			JSONObject courseSectionObject = courseSectionListObject.get(i).isObject();
+		//Add checks for null values?
+	
+		// TODO look into time conversion more
+		// put into JSONHelper?
 		
-			Integer id = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.ID));
-			String sectionName = JSONHelper.getStringValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.SECTION_NAME));
-			Integer sectionId = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.SECTION_ID));
-			String sectionType = JSONHelper.getStringValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.SECTION_TYPE));
-			Integer population = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.POPULATION));
-			Integer courseId = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.COURSE_ID));
-			Integer instructorId = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.INSTRUCTOR_ID));
-			Integer calendarInfoId = JSONHelper.getIntValue(courseSectionObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.CALENDAR_INFO_ID));
-			
-			//Add checks for null values?
-		
-			// TODO look into time conversion more
-			// put into JSONHelper?
-			
-			courseSection.setId(id);
-			courseSection.setSectionName(sectionName);
-			courseSection.setSectionId(sectionId);
-			courseSection.setSectionType(sectionType);
-			courseSection.setPopulation(population);
-			courseSection.setCourseId(courseId);
-			courseSection.setInstructorId(instructorId);
-			courseSection.setCalendarInfoId(calendarInfoId);			
-		}
+		courseSection.setId(id);
+		courseSection.setSectionName(sectionName);
+		courseSection.setSectionId(sectionId);
+		courseSection.setSectionType(sectionType);
+		courseSection.setPopulation(population);
+		courseSection.setCourseId(courseId);
+		courseSection.setInstructorId(instructorId);
+		courseSection.setCalendarInfoId(calendarInfoId);			
 		
 		//CalendarInfo object
 		
 		JSONValue calendarInfoObjectStart = returnEditSectionObject.get("calendarInfo");
-		JSONArray calendarInfoListObject = calendarInfoObjectStart.isArray();
+		JSONObject calendarInfoObject = calendarInfoObjectStart.isObject();
 		
 		CalendarInfo calendarInfo = new CalendarInfo();
+			
+		Integer calInfoId = JSONHelper.getIntValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveCalendarInfoKeys.ID));		
+		Integer calYear = JSONHelper.getIntValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveCalendarInfoKeys.CAL_YEAR));
+		String calTerm = JSONHelper.getStringValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveCalendarInfoKeys.CAL_TERM));
+		String days = JSONHelper.getStringValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveCalendarInfoKeys.DAYS));
+		Integer startTime = JSONHelper.getIntValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveCalendarInfoKeys.START_TIME));
+		Integer endTime = JSONHelper.getIntValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveCalendarInfoKeys.END_TIME));
+	
+		// TODO look into time conversion more
+		// put into JSONHelper?
 		
-		for (int i = 0; i < calendarInfoListObject.size(); i++) {
-			
-			JSONObject calendarInfoObject = calendarInfoListObject.get(i).isObject();
-			
-			Integer id = JSONHelper.getIntValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.CALENDAR_INFO_ID));		
-			Integer calYear = JSONHelper.getIntValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.CAL_YEAR));
-			String calTerm = JSONHelper.getStringValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.CAL_TERM));
-			String days = JSONHelper.getStringValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.DAYS));
-			Integer startTime = JSONHelper.getIntValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.START_TIME));
-			Integer endTime = JSONHelper.getIntValue(calendarInfoObject, JSONHelper.convertKeyName(ReceiveEditSectionKeys.END_TIME));
-		
-			// TODO look into time conversion more
-			// put into JSONHelper?
-			
-			calendarInfo.setId(id);
-			calendarInfo.setCalYear(calYear);
-			calendarInfo.setCalTerm(calTerm);
-			calendarInfo.setDays(days);
-			calendarInfo.setStartTime(startTime);
-			calendarInfo.setEndTime(endTime);		
-		}
+		calendarInfo.setId(id);
+		calendarInfo.setCalYear(calYear);
+		calendarInfo.setCalTerm(calTerm);
+		calendarInfo.setDays(days);
+		calendarInfo.setStartTime(startTime);
+		calendarInfo.setEndTime(endTime);		
 		
 		// possibly use builder pattern if it is a lot of data
 		
